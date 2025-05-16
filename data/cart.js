a69671ca8993cc4f962b6ef8cf98f39d57e240df
revanmk <revanmk08@gmail.com>
2025-05-16 22:56:42 +0530

    Moved addToCart into cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,22 @@
 export const cart=[];
+
+
+export function addToCart(productId){
+    const select_quantity=Number(document.querySelector(`.js-quantity-selector-${productId}`).value);
+            let matchingItem;
+            cart.forEach((cartItem)=>{
+                if(productId===cartItem.productId){
+                    matchingItem=cartItem;
+                }
+            });
+            
+            if(matchingItem){
+                matchingItem.quantity+=select_quantity;
+            }
+            else{
+            cart.push({
+                productId:productId,
+                quantity:select_quantity
+            });
+        }  
+}
