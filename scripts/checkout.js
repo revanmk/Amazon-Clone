29852a0ed83b441ca8ec2c0325488241b02b305f
revanmk <revanmk08@gmail.com>
2025-05-29 23:55:04 +0530

    Learn promises and convert code into promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,9 +1,24 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProducts } from "../data/products.js";
+import { loadCart } from "../data/cart.js";
 //import '../data/cart-class.js';
 //import '../data/backend-practice.js';
-loadProducts(()=>{
+
+
+
+Promise.all([
+    new Promise((resolve)=>{
+        loadProducts(()=>{
+            resolve();
+        })
+    }),
+    new Promise((resolve)=>{
+        loadCart(()=>{
+            resolve();
+        });
+    })
+]).then(()=>{
     renderOrderSummary();
     renderPaymentSummary();
-});
+})
