4a4b86ed7c3109c026736ec37ce4f7b2cad3c8a1
revanmk <revanmk08@gmail.com>
2025-05-29 23:27:58 +0530

    Load products in beforeAll hook

--- tests-jasmine/checkout/orderSummaryTest.js
+++ tests-jasmine/checkout/orderSummaryTest.js
@@ -1,8 +1,16 @@
 import{renderOrderSummary} from '../../scripts/checkout/orderSummary.js'
 import {loadFromStorage} from '../../data/cart.js';
-
+import { loadProducts } from '../../data/products.js';
 
 describe('test suite: renderOrderSummary',()=>{
+
+
+    beforeAll((done)=>{
+        loadProducts(()=>{
+            done();
+        });
+    })
+    
     it('displays the cart',()=>{
         document.querySelector('.js-test-container').innerHTML=`
             <div class="js-order-summary"></div>
