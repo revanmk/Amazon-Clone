c5adf2a4816b50a4f49965c7527865a858066a49
revanmk <revanmk08@gmail.com>
2025-05-30 14:48:06 +0530

    Convert loadProducts from xhr to fetch

--- data/products.js
+++ data/products.js
@@ -714,6 +714,28 @@ class Clothing extends Product{
 
 export let products=[];
 
+
+export function loadProductsFetch(){
+  const promise=fetch('https://supersimplebackend.dev/products').then((response)=>{
+    return response.json();
+  }).then((productsData)=>{
+    console.log(productsData);
+    products=productsData.map((productDetails)=>{
+      if(productDetails.type=='clothing')
+        return new Clothing(productDetails);
+      else
+      return new Product(productDetails);
+      });
+      console.log('load Products');
+      
+  })
+  return promise;
+}
+// }
+// loadProductsFetch().then(()=>{
+//   console.log('next step');
+// });
+
 export function loadProducts(fun){
     const xhr=new XMLHttpRequest();
 
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,6 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
-import { loadProducts } from "../data/products.js";
+import { loadProducts,loadProductsFetch } from "../data/products.js";
 import { loadCart } from "../data/cart.js";
 //import '../data/cart-class.js';
 //import '../data/backend-practice.js';
@@ -8,11 +8,7 @@ import { loadCart } from "../data/cart.js";
 
 
 Promise.all([
-    new Promise((resolve)=>{
-        loadProducts(()=>{
-            resolve();
-        })
-    }),
+    loadProductsFetch(),
     new Promise((resolve)=>{
         loadCart(()=>{
             resolve();
